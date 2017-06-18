140409306be36e07d35033c82e01846cced40c10
gpbacang <gpbacang@Geraldines-MacBook-Pro.local>
2017-06-18 18:55:47 +0800

    Add header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { HeaderComponent } from './header/header.component';
 
 import { AppComponent } from './app.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule,
